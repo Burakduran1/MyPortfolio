be4c6e8dbcbe531cf5f4976a1d1e15d58a6dd3b5
Burak Can Duran <burakduran12491@gmail.com>
2024-05-05 00:57:00 +0300

    Second Commit

---
+++ MyPortfolio/DAL/Entitites/About.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolio.DAL.Entitites
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+        public string  Details { get; set; }
+        public string SubDescription { get; set; }
+    }
+}
---
+++ MyPortfolio/DAL/Entitites/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolio.DAL.Entitites
+{
+    public class Skill
+    {
+        public int SkillId { get; set; }
+        public string Title { get; set; }
+        public int Value { get; set; }
+    }
+}
