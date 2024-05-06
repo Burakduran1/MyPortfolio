6d6d37712571e8227f73674ef02c570dfcdab964
Burak Can Duran <burakduran12491@gmail.com>
2024-05-06 16:30:31 +0300

    Admin Panel

---
+++ MyPortfolio/Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        public MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IActionResult Inbox()
+        {
+            var values = portfolioContext.Messages.ToList();
+            return View(values);
+        }
+    }
+}
