a077410f669681dc71b104cb088f382175aeef7f
Burak Can Duran <burakduran12491@gmail.com>
2024-05-09 00:47:26 +0300

    __

--- MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
+++ MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
-namespace MyPortfolio.ViewComponents.LayoutViewComponents
+
+namespace MyPortolioUdemy.ViewComponents.LayoutViewComponents
 {
-	public class _LayoutNavbarComponentPartial : ViewComponent
-	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+    public class _LayoutNavbarComponentPartial : ViewComponent
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.toDoListCount = portfolioContext.ToDoLists.Where(x => x.Status == false).Count();
+            var values = portfolioContext.ToDoLists.Where(x => x.Status == false).ToList();
+            return View(values);
+        }
+    }
 }
